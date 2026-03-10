18cc83a9198428b596978b8611d8f083a6b1fe98
Sandro Rodriguez Anchante <sandro661528@gmail.com>
2026-03-10 17:37:53 -0500

    Tablas y Semillas de Servicios

--- app/Database/Seeds/DatabaseSeeder.php
+++ app/Database/Seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsuariosSeeder::class);
         $this->call(EmpresasSeeder::class);
         $this->call(ResponsablesEmpresasSeeder::class);
+        $this->call(ServiciosSeeder::class);
     }
 }
